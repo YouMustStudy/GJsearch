2a3af62a3e6f076283bb7a3ffee376b5a41287f9
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2019-05-12 18:41:53 +0900

    시군 콤보박스 및 구동 엔트리 생성

--- GJsearch.py
+++ GJsearch.py
@@ -1,13 +1,30 @@
 from tkinter import *
+from tkinter import ttk
+
+sigun = ["안양시", "과천시", "흥흥"]
 
 class GJsearch:
-    width = 400
-    height = 200
+    width = 800
+    height = 600
     def __init__(self):
+        #Tk 기본설정
         root = Tk()
+        root.configure(background='white')
         root.geometry(str(GJsearch.width)+"x"+str(GJsearch.height))
         root.resizable(width = False, height = False)
         root.title("GJsearch")
+
+        #시/군 콤보박스 생성
+        Label(text="시/군").place(x=150, y=20)
+        self.sigunData = StringVar()
+        self.sigunList = ttk.Combobox(root, textvariable = self.sigunData, width = 7)
+        self.sigunList['values'] = sigun
+        self.sigunList.place(x=150, y=40)
+
+        #구/동 엔트리 생성
+        Label(text="구/동/읍/면").place(x=250, y=20)
+        self.gudongData = Entry(root, width=10)
+        self.gudongData.place(x=250, y=40)
 
         root.mainloop()
 
