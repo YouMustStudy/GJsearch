6a1c59af2a14a3d0f887171316453a040d685cb8
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2019-05-12 19:57:19 +0900

    UI 수정

--- GJsearch.py
+++ GJsearch.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 sigun = ["안양시", "과천시", "흥흥"]
 
 class GJsearch:
-    width = 800
+    width = 600
     height = 600
     def __init__(self):
         #Tk 기본설정
@@ -15,30 +15,31 @@ class GJsearch:
         root.title("GJsearch")
 
         #시/군 콤보박스 생성
-        Label(text="시/군").place(x=10, y=60)
+        Label(text="시/군", background='white').place(x=10, y=110)
         self.sigunData = StringVar()
         self.sigunList = ttk.Combobox(root, textvariable = self.sigunData, width = 7)
         self.sigunList['values'] = sigun
-        self.sigunList.place(x=10, y=80)
+        self.sigunList.place(x=10, y=130)
 
         #구/동 엔트리 생성
-        Label(text="동/읍/면").place(x=110, y=60)
+        Label(text="동/읍/면", background='white').place(x=120, y=110)
         self.gudongData = Entry(root, width=10)
-        self.gudongData.place(x=110, y=80)
+        self.gudongData.place(x=120, y=130)
 
         #검색 버튼
         self.search = Button(root, text="검색")
-        self.search.place(x=200, y=80)
+        self.search.place(x=220, y=125)
 
         #회사 검색결과 리스트박스
-        Label(text="게임회사").place(x=10, y=130)
+        Label(text="게임회사", background='white').place(x=10, y=180)
         self.searchList = Listbox(root, width= 15, height= 20)
-        self.searchList.place(x=10, y=150)
+        self.searchList.place(x=10, y=200)
+        
 
         #채용공고 검색결과 리스트박스
-        Label(text="채용공고").place(x=120, y=130)
+        Label(text="채용공고", background='white').place(x=120, y=180)
         self.searchList = Listbox(root, width= 20, height= 20)
-        self.searchList.place(x=120, y=150)
+        self.searchList.place(x=120, y=200)
 
         #회사 정보
         Label(text="회사정보", width = 40, height = 6).place(x=300, y=100)
