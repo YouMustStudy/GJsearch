d377c07686ddc01d80eb5f3a42672c199cae2d8f
YouMustStudy <43169744+YouMustStudy@users.noreply.github.com>
2019-06-17 22:52:02 +0900

    텔레그램 오류 수정

--- teller.py
+++ teller.py
@@ -41,15 +41,15 @@ def handle(msg):
 
     if text.startswith('검색') and len(args)>1:
         print('try to 검색', args[1], 0)
-        replyAptData( chat_id, args[1] )
+        replyAptData( chat_id, args[1], 0)
     elif text.startswith('공고') and len(args)>1:
         print('try to 공고', args[1], 1)
-        replyAptData(chat_id, args[1])
+        replyAptData(chat_id, args[1], 1)
     elif text.startswith('지원'):
         print('try to 지원')
         noti.sendMessage(chat_id, '지원 도시 : 수원시, 성남시, 의정부시, 안양시, 부천시, 광명시, 평택시, 동두천시, 안산시, 고양시, 과천시, 구리시, 남양주시, 오산시, 시흥시, 군포시, 의왕시, 하남시, 용인시, 파주시, 이천시, 안성시, 김포시, 화성시, 광주시, 양주시, 포천시, 여주시, 연천군, 가평군, 양평군')
     else:
-        noti.sendMessage(chat_id, '모르는 명령어입니다.\n검색 [도시명], 지원 중 하나의 명령을 입력하세요.')
+        noti.sendMessage(chat_id, '모르는 명령어입니다.\n검색 [도시명], 공고 [회사명], 지원 중 하나의 명령을 입력하세요.')
 
 
 today = date.today()
